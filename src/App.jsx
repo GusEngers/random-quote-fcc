036b69de1ba6e3f9b376803385d315cc966dd1c3
GusEngers <gustavoengers6441@gmail.com>
2024-06-04 15:20:12 -0300

    a

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
         href="https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=%22Few%20things%20can%20help%20an%20individual%20more%20than%20to%20place%20responsibility%20on%20him%2C%20and%20to%20let%20him%20know%20that%20you%20trust%20him.%22%20Booker%20T.%20Washington"
         id="tweet-quote"
         target='_blank'
-        >Tweet</a>
+        >Twee</a>
     </div>
   );
 }
