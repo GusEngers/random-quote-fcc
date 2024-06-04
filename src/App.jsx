3d6bfc8691cbd832db634a8c780e6ac26063d633
GusEngers <gustavoengers6441@gmail.com>
2024-06-04 15:16:53 -0300

    [ADD] Maquetado inicial de la estructura de la vista

--- src/App.jsx
+++ src/App.jsx
@@ -10,9 +10,16 @@ function App() {
   }, []);
 
   return (
-    <>
-      <h1>Hola Mundo</h1>
-    </>
+    <div id='quote-box'>
+      <p id="text">Hola</p>
+      <p id="author">Autor</p>
+      <button id="new-quote">NEW QUOTE</button>
+      <a 
+        href="https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=%22Few%20things%20can%20help%20an%20individual%20more%20than%20to%20place%20responsibility%20on%20him%2C%20and%20to%20let%20him%20know%20that%20you%20trust%20him.%22%20Booker%20T.%20Washington"
+        id="tweet-quote"
+        target='_blank'
+        >Tweet</a>
+    </div>
   );
 }
 
